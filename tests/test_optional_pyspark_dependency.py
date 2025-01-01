5da7e9fbc1e9a380d6fed0e644e4cc4f2f763ea0
mitchstockdale <mitchellastockdale@gmail.com>
2025-01-02 08:13:48 +1100

    [squash] Require minimum pyspark version

--- tests/test_optional_pyspark_dependency.py
+++ tests/test_optional_pyspark_dependency.py
@@ -1,16 +1,17 @@
 import pydantic
 import pytest
 
-from sparkdantic import model
+from sparkdantic import model, utils
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def no_pyspark(monkeypatch):
-    monkeypatch.setattr(model, '_have_pyspark', False)
-    monkeypatch.setattr(model, '_pyspark_import_error', ImportError('No module named pyspark'))
+    for module in [utils, model]:
+        monkeypatch.setattr(module, 'have_pyspark', False)
+        monkeypatch.setattr(module, 'pyspark_import_error', ImportError('No module named pyspark'))
 
 
-def test_create_spark_schema_raises_import_error_when_no_pyspark():
+def test_create_spark_schema_raises_import_error_when_no_pyspark(no_pyspark):
     class NoPySparkModel(pydantic.BaseModel):
         pass
 
@@ -19,10 +20,51 @@ def test_create_spark_schema_raises_import_error_when_no_pyspark():
     assert 'No module named pyspark' == str(exc.value)
 
 
-def test_model_spark_schema_raises_import_error_when_no_pyspark():
+def test_model_spark_schema_raises_import_error_when_no_pyspark(no_pyspark):
     class NoPySparkModel(model.SparkModel):
         pass
 
     with pytest.raises(ImportError) as exc:
         NoPySparkModel.model_spark_schema()
     assert 'No module named pyspark' == str(exc.value)
+
+
+def test_no_pyspark_raises_import_error(no_pyspark):
+    with pytest.raises(ImportError) as exc:
+        utils.require_minimum_pyspark_version()
+    assert 'No module named pyspark' == str(exc.value)
+
+
+def test_lower_pyspark_version_raises_import_error(
+    monkeypatch,
+):
+    low_version = '3.2.0'
+    monkeypatch.setattr(utils.pyspark, '__version__', low_version)
+    monkeypatch.setattr(
+        utils,
+        'pyspark_import_error',
+        ImportError(f'PySpark version 3.3.0 or newer is required, but found {low_version}'),
+    )
+
+    with pytest.raises(ImportError) as exc:
+        utils.require_minimum_pyspark_version()
+    assert 'PySpark version 3.3.0 or newer is required, but found 3.2.0' == str(exc.value)
+
+
+@pytest.mark.parametrize(
+    'version',
+    [
+        '3.3.0',
+        '3.3.1',
+        '3.4.0',
+        '4.0.0',
+    ],
+)
+def test_at_least_minimum_pyspark_version_does_not_raise_error(
+    version,
+    monkeypatch,
+):
+    monkeypatch.setattr(utils, 'have_pyspark', True)
+    monkeypatch.setattr(utils.pyspark, '__version__', version)
+
+    utils.require_minimum_pyspark_version()
